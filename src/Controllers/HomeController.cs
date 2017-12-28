92aa8c9a4048cb8ffb4c90a0796170f396faf0c4
Matias Quaranta <ealsur@hotmail.com>
2017-12-28 16:15:35 -0300

    Adding landing page

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿namespace loadtesting.Controllers
+{
+    using loadtesting.Services;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        private readonly ITestDocumentService _testDocumentService;
+        public HomeController(ITestDocumentService testDocumentService)
+        {
+            this._testDocumentService = testDocumentService;
+        }
+
+        public ActionResult Index()
+        {
+
+            return View("Index", _testDocumentService.GetDocument().ToString(Newtonsoft.Json.Formatting.Indented));
+        }
+    }
+}
